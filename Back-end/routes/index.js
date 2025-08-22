a0465ac1dfa9ae6c67782094219353232bce9386
K DANUSAN <danusan.official@gmail.com>
2025-08-22 08:48:09 +0530

    index js update 1

--- Back-end/routes/index.js
+++ Back-end/routes/index.js
@@ -8,6 +8,8 @@ const projectRoutes = require('./projects');
 const defectRoutes = require('./defects');
 const lookupRoutes = require('./lookups');
 const configRoutes = require('./config');
+const dashboardRoutes = require('./dashboard');
+const releaseRoutes = require('./releases');
 
 // Route definitions
 router.use('/auth', authRoutes);
@@ -16,6 +18,8 @@ router.use('/projects', projectRoutes);
 router.use('/projects', defectRoutes);
 router.use('/', lookupRoutes);
 router.use('/config', configRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/releases', releaseRoutes);
 
 // Health check endpoint
 router.get('/health', (req, res) => {
