466afac044b31836e28ca84e3e6706dc0302e50c
K DANUSAN <danusan.official@gmail.com>
2025-08-22 08:47:46 +0530

    project js CRUD update 1

--- Back-end/routes/projects.js
+++ Back-end/routes/projects.js
@@ -9,8 +9,8 @@ const { validateProject, validateModule, validateId, validateProjectId, handleVa
 router.use(authenticateToken);
 
 // Project routes
-router.get('/', checkPrivilege('projects', 'READ'), projectController.getAllProjects);
-router.post('/', validateProject, handleValidationErrors, checkPrivilege('projects', 'CREATE'), projectController.createProject);
+router.get('/', projectController.getAllProjects);
+router.post('/', validateProject, handleValidationErrors, projectController.createProject);
 
 // Project-specific routes (require project access)
 router.get('/:projectId', validateProjectId, handleValidationErrors, checkProjectAccess, projectController.getProjectById);
@@ -19,22 +19,22 @@ router.delete('/:projectId', validateProjectId, handleValidationErrors, checkPro
 
 // Module routes
 router.get('/:projectId/modules', validateProjectId, handleValidationErrors, checkProjectAccess, moduleController.getModules);
-router.post('/:projectId/modules', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'UPDATE', true), moduleController.createModule);
+router.post('/:projectId/modules', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, moduleController.createModule);
 router.get('/:projectId/modules/:id', validateProjectId, validateId, handleValidationErrors, checkProjectAccess, moduleController.getModuleById);
 router.put('/:projectId/modules/:id', validateProjectId, validateId, validateModule, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'UPDATE', true), moduleController.updateModule);
-router.delete('/:projectId/modules/:id', validateProjectId, validateId, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'DELETE', true), moduleController.deleteModule);
+router.delete('/:projectId/modules/:id', validateProjectId, validateId, handleValidationErrors, checkProjectAccess, moduleController.deleteModule);
 
 // Submodule routes
 router.get('/:projectId/modules/:moduleId/submodules', validateProjectId, handleValidationErrors, checkProjectAccess, moduleController.getSubModules);
-router.post('/:projectId/modules/:moduleId/submodules', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'UPDATE', true), moduleController.createSubModule);
-router.put('/:projectId/modules/:moduleId/submodules/:subModuleId', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'UPDATE', true), moduleController.updateSubModule);
-router.delete('/:projectId/modules/:moduleId/submodules/:subModuleId', validateProjectId, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'DELETE', true), moduleController.deleteSubModule);
+router.post('/:projectId/modules/:moduleId/submodules', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, moduleController.createSubModule);
+router.put('/:projectId/modules/:moduleId/submodules/:subModuleId', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, moduleController.updateSubModule);
+router.delete('/:projectId/modules/:moduleId/submodules/:subModuleId', validateProjectId, validateModule, handleValidationErrors, checkProjectAccess, moduleController.deleteSubModule);
 
 // Project allocation routes
 router.get('/:projectId/allocations', validateProjectId, handleValidationErrors, checkProjectAccess, projectController.getProjectAllocations);
-router.post('/:projectId/allocations', validateProjectId, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'MANAGE', true), projectController.allocateUserToProject);
-router.put('/:projectId/allocations/:allocationId', validateProjectId, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'MANAGE', true), projectController.updateProjectAllocation);
-router.delete('/:projectId/allocations/:allocationId', validateProjectId, handleValidationErrors, checkProjectAccess, checkPrivilege('projects', 'MANAGE', true), projectController.deallocateUser);
+router.post('/:projectId/allocations', validateProjectId, handleValidationErrors, checkProjectAccess, projectController.allocateUserToProject);
+router.put('/:projectId/allocations/:allocationId', validateProjectId, handleValidationErrors, checkProjectAccess, projectController.updateProjectAllocation);
+router.delete('/:projectId/allocations/:allocationId', validateProjectId, handleValidationErrors, checkProjectAccess, projectController.deallocateUser);
 router.get('/:projectId/allocation-history', validateProjectId, handleValidationErrors, checkProjectAccess, projectController.getAllocationHistory);
 
 module.exports = router;
